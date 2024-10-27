aec254cf162dc6cdb6fcef0660287f74a56ef37f
fnfpwp <117086223+pwp114514@users.noreply.github.com>
2024-10-27 08:52:17 +0800

    Update JukeboxState.hx

--- source/JukeboxState.hx
+++ source/JukeboxState.hx
@@ -170,7 +170,7 @@ class JukeboxState extends MusicBeatState
 		// bg.updateHitbox();
 		bgDiamonds.updateHitbox();
 		var scrollDiamonds:FlxBackdrop;
-		scrollDiamonds = new FlxBackdrop(bgDiamonds.graphic, 60, 60, true, true);
+		scrollDiamonds = new FlxBackdrop(bgDiamonds.graphic, 60, 60);
 		scrollDiamonds.scrollFactor.set(0, 0);
 		scrollDiamonds.blend = BlendMode.MULTIPLY;
 		scrollDiamonds.alpha = 0.1;
